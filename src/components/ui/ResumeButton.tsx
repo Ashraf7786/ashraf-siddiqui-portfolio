1ad76a90e10216d6ec8ce16321a8727c79bb195d
Siddiquiashraf <ashrafsiddiqui6378@gmail.com>
2026-05-12 14:54:29 +0530

    fix: rename resume to Ashraf-Resume-2026.pdf to fix download error

--- src/components/ui/ResumeButton.tsx
+++ src/components/ui/ResumeButton.tsx
@@ -32,8 +32,8 @@ export default function ResumeButton() {
     const handleDownload = () => {
         // Trigger actual download
         const link = document.createElement('a');
-        link.href = '/Ashraf%20Resume%202026_.pdf'; // Path to the resume file
-        link.download = 'Ashraf Resume 2026_.pdf';
+        link.href = '/Ashraf-Resume-2026.pdf'; // Path to the resume file
+        link.download = 'Ashraf-Resume-2026.pdf';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
